a4ede6ade97a5b6d5cced79eac68de9b8ffe17f3
pandurengaraj.b <pandurengaraj.b@sybrant.com>
2013-05-10 16:14:49 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pandubalraj::Application.config.secret_token = '2419137c19883c6d29bc6c1ccda51d229f627a3ecae5489085daa2ae47822db3e50ff2af899a058ce045a15765f5b61cdbf1ab51c9e36fec53c85aa1577907aa'
